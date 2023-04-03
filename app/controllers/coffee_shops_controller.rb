5a8c8bf217c23d2eac32509ee2d23a1284a95946
“Kathleen <kgmcdonnell@mail.usf.edu>
2023-04-03 09:46:39 -0400

    created index,show,and create actions in coffee shop controller

--- app/controllers/coffee_shops_controller.rb
+++ app/controllers/coffee_shops_controller.rb
@@ -1,2 +1,28 @@
 class CoffeeShopsController < ApplicationController
+  def index
+    @coffee_shops = CoffeeShop.all
+    render json: @coffee_shops.as_json
+  end
+
+  def show
+    @coffee_shop = CoffeeShop.find_by(id: params[:id])
+    render json: @coffee_shop.as_json
+  end
+
+  def create
+    @coffee_shop = CoffeeShop.new(
+      name: params[:name],
+      city: params[:city],
+      state: params[:state],
+      address: params[:address],
+      number: params[:number],
+      logo: params[:logo],
+    )
+
+    if @coffee_shop.save
+      render json: @coffee_shop.as_json
+    else
+      render json: { errors: @coffee_shop.errors.full_messages }, status: :bad_request
+    end
+  end
 end
