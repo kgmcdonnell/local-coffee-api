1292ed3ddc8871a81161f3411ef293b284f8d381
“Kathleen <kgmcdonnell@mail.usf.edu>
2023-04-03 10:15:06 -0400

    created user create and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+  def create
+    @user = User.new(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation],
+    )
+
+    if @user.save
+      render json: { message: "User created!" }, status: :created
+    else
+      render json: { errors: @user.errors.full_messages }, request: :bad_request
+    end
+  end
+
+  def destroy
+    @user = User.find_by(id: params[:id])
+    @user.destroy
+    render json: { message: "User deleted!" }, request: 200
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   resources :coffee_shops
+  resources :users
 end
