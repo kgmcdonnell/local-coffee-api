f381f6df9cd528b9f9e9d695f51c6389cbe38249
“Kathleen <kgmcdonnell@mail.usf.edu>
2023-04-11 14:03:49 -0400

    updated users table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user, except: [:create]
+
   # User Sign-Up
   def create
     @user = User.new(
@@ -7,6 +8,7 @@ class UsersController < ApplicationController
       email: params[:email],
       password: params[:password],
       password_confirmation: params[:password_confirmation],
+
     )
 
     if @user.save
@@ -16,12 +18,25 @@ class UsersController < ApplicationController
     end
   end
 
+  # Update User Profile Picture
+  def update
+    @user = User.find_by(id: params[:id])
+    if @user == current_user
+      @user.update(
+        picture: params[:picture] || @user.picture,
+      )
+      render json: { message: "User updated!" }, status: 200
+    else
+      render json: { errors: "Unauthorized" }, status: 401
+    end
+  end
+
   # User Deletes their profile
   def destroy
     @user = User.find_by(id: params[:id])
     if @user == current_user
       @user.destroy
-      render json: { message: "User deleted!" }, request: 200
+      render json: { message: "User deleted!" }, status: 200
     else
       render json: { errors: "Unauthorized" }, status: 401
     end
