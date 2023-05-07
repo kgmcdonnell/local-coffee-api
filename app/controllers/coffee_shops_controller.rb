a5e39dd3c58788a03dfeabb7784ee16a647e91aa
“Kathleen <kgmcdonnell@mail.usf.edu>
2023-05-07 16:19:12 -0400

    added parameters to google places details api

--- app/controllers/coffee_shops_controller.rb
+++ app/controllers/coffee_shops_controller.rb
@@ -17,7 +17,7 @@ class CoffeeShopsController < ApplicationController
   # show a specific coffee shop
   def show
     id = params[:id]
-    response = HTTP.get("https://maps.googleapis.com/maps/api/place/details/json?&place_id=#{id}&fields=name,formatted_address,opening_hours,formatted_phone_number,rating,website&key=#{ENV["GOOGLE_PLACES_API_KEY"]}")
+    response = HTTP.get("https://maps.googleapis.com/maps/api/place/details/json?&place_id=#{id}&fields=name,formatted_address,opening_hours,formatted_phone_number,rating,website,photos,reviews&key=#{ENV["GOOGLE_PLACES_API_KEY"]}")
     @coffee_shop = response.parse(:json)
     render json: @coffee_shop
   end
